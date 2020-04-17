4fa6942d2c0d919adc18644ef0ecd988396ce68b
Falola Tele <only1tele@gmail.com>
2020-04-18 00:15:38 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,212 @@
+<template>
+<div>
+  <section class="hero">
+
+  </section>
+  <section class="flash">
+    <div class="flash__left">
+      Deals
+    </div>
+    <div class="flash__right">
+      Sales
+    </div>
+
+  </section>
+  <section class="featured__small">
+      <div class="title">
+          Featured Products
+      </div>
+      <div class="featured__box">
+        <div class="box">
+          <img src="~/assets/images/hero.png" class="image">
+          <div class="box__title">
+            Simpsons Max Sofa House
+Chair
+          </div>
+          <div class="box__amount">
+            ₦80,000
+          </div>
+          <div class="box__rating">
+            *****
+          </div>
+        </div>
+        <div class="box">
+           <img src="~/assets/images/hero.png" class="image">
+          <div class="box__title">
+            Simpsons Max Sofa House
+Chair
+          </div>
+          <div class="box__amount">
+            ₦80,000
+          </div>
+          <div class="box__rating">
+            *****
+          </div>
+        </div>
+        <div class="box">
+           <img src="~/assets/images/hero.png" class="image">
+          <div class="box__title">
+            Simpsons Max Sofa House
+Chair
+          </div>
+          <div class="box__amount">
+            ₦80,000
+          </div>
+          <div class="box__rating">
+            *****
+          </div>
+        </div>
+        <div class="box">
+           <img src="~/assets/images/hero.png" class="image">
+          <div class="box__title">
+            Simpsons Max Sofa House
+Chair
+          </div>
+          <div class="box__amount">
+            ₦80,000
+          </div>
+          <div class="box__rating">
+            *****
+          </div>
+        </div>
+      </div>
+  </section>
+  <section class="featured__large">
+    <div class="title">
+          Featured Products
+    </div>
+    <div class="featured__box">
+      <div class="box">
+       <div class="box__title">
+         FURNITURE
+       </div>
+       <img src="~/assets/images/hero.png" class="image">
+       <div class="image__container">
+          <div class="image__box">
+            <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+       </div>
+      </div>
+       <div class="box">
+       <div class="box__title">
+         OUTDOOR
+       </div>
+       <img src="~/assets/images/hero.png" class="image">
+       <div class="image__container">
+          <div class="image__box">
+            <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+       </div>
+      </div>
+       <div class="box">
+       <div class="box__title">
+         DECOR
+       </div>
+       <img src="~/assets/images/hero.png" class="image">
+       <div class="image__container">
+          <div class="image__box">
+            <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+       </div>
+      </div>
+       <div class="box">
+       <div class="box__title">
+         STORAGE
+       </div>
+       <img src="~/assets/images/hero.png" class="image">
+       <div class="image__container">
+          <div class="image__box">
+            <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+       </div>
+      </div>
+      <div class="box">
+       <div class="box__title">
+         KITCHEN
+       </div>
+       <img src="~/assets/images/hero.png" class="image">
+       <div class="image__container">
+          <div class="image__box">
+            <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+       </div>
+      </div>
+       <div class="box">
+       <div class="box__title">
+         APPLIANCES
+       </div>
+       <img src="~/assets/images/hero.png" class="image">
+       <div class="image__container">
+          <div class="image__box">
+            <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+          <div class="image__box">
+           <img src="~/assets/images/hero.png" class="image">
+          </div>
+       </div>
+      </div>
+    </div>
+  </section>
+  <section class="slide">
+    <div class="content">
+      dd
+    </div>
+     <div class="image">
+       <img src="~/assets/images/hero.png" class="slide__image">
+    </div>
+  </section>
+  <section class="newsletter">
+    <h5>Be in the know</h5>
+    <p>Get recommendations, updates, promotions and more</p>
+    <div class="form__control">
+      <input type="text"/>
+      <button>Sign up</button>
+    </div>
+  </section>
+</div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  }
+}
+</script>
+
+<style>
+
+</style>
